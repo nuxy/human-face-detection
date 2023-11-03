53f9e803cfc1f68d8f39f277c24a900835a048a9
Marc S. Brooks <marc@mbrooks.info>
2023-11-03 11:56:45 -0700

    Added Mocha environment overrides

--- test/mocha.env.js
+++ test/mocha.env.js
@@ -1,2 +1,9 @@
 // Global shorthand.
 global.PACKAGE_ROOT = process.cwd();
+
+// Environment overrides.
+process.env.HUMAN_DEBUG      = false;
+process.env.HUMAN_MODELS_DIR = 'node_modules/@vladmandic/human/models';
+
+// TensorFlow logging
+process.env.TF_CPP_MIN_LOG_LEVEL = 3;
